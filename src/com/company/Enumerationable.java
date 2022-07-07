c9d001a259c1daef9180e05bb58b6feafa8cc60f
Maksim Shvechkov <shvechkov_maks@mail.ru>
2022-07-07 18:52:07 +0300

    Added file Enumerationable.java

---
+++ src/com/company/Enumerationable.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public interface Enumerationable {
+    void openConnection();
+    void closeConnection();
+    void checkDataBaseConnection();
+    void readLineIndex();
+    void checkKeyIndex();
+    void readKey();
+    void readKeyCollection();
+    void getNumberOfRecords();
+    void addRecordToBase();
+    void renewValueIndex();
+    void renewKeyValue();
+}
